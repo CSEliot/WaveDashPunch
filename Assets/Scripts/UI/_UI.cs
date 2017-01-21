6e27027b0101df1dc3ab185f2032c4b10df9bfde
ecsit39 <ecsit39@gmail.com>
2017-01-21 14:46:09 -0500

    Created UI Manager and an easy static reference. And Controls + Network scenes have merged.

---
+++ Assets/Scripts/UI/_UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class _UI {
+
+    /// <summary>
+    /// Set the HP Bar of the player.
+    /// </summary>
+    /// <param name="Percentage">from 0 to 1.0f</param>
+    public static void SetHPBar(float Percentage)
+    {
+        GameObject.FindGameObjectWithTag("UI").GetComponent<UIManager>().SetHPBar(Percentage);
+    }
+    
+    /// <summary>
+     /// Set the Jetpack Fuel Bar of the player.
+     /// </summary>
+     /// <param name="Percentage">from 0 to 1.0f</param>
+    public static void SetJetBar(float Percentage)
+    {
+        GameObject.FindGameObjectWithTag("UI").GetComponent<UIManager>().SetJetBar(Percentage);
+    }
+}
