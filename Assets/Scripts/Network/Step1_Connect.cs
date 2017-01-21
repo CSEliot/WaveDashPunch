6b654f950d9f0179d21b96fe41f7bd6aa5b80b04
ecsit39 <ecsit39@gmail.com>
2017-01-21 18:00:01 -0500

    Added lighting data and switched back to random room join.

--- Assets/Scripts/Network/Step1_Connect.cs
+++ Assets/Scripts/Network/Step1_Connect.cs
@@ -13,8 +13,6 @@ public class Step1_Connect : Photon.MonoBehaviour
     /// <summary>Connect automatically? If false you can set this to true later on or call ConnectUsingSettings in your own scripts.</summary>
     public bool AutoConnect = true;
 
-    public byte Version = 1;
-
     /// <summary>if we don't want to connect in Start(), we have to "remember" if we called ConnectUsingSettings()</summary>
     private bool ConnectInUpdate = true;
 
@@ -31,7 +29,7 @@ public class Step1_Connect : Photon.MonoBehaviour
             Debug.Log("Update() was called by Unity. Scene is loaded. Let's connect to the Photon Master Server. Calling: PhotonNetwork.ConnectUsingSettings();");
 
             ConnectInUpdate = false;
-            PhotonNetwork.ConnectUsingSettings(Version + "." + SceneManagerHelper.ActiveSceneBuildIndex);
+            PhotonNetwork.ConnectUsingSettings("WaveDashRun_v" + Application.version);
         }
     }
 
@@ -43,16 +41,16 @@ public class Step1_Connect : Photon.MonoBehaviour
     public virtual void OnConnectedToMaster()
     {
         Debug.Log("OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room. Calling: PhotonNetwork.JoinRandomRoom();");
-        PhotonNetwork.JoinRoom("WaveDashRun");
+        PhotonNetwork.JoinRandomRoom();
     }
 
     public virtual void OnJoinedLobby()
     {
         Debug.Log("OnJoinedLobby(). This client is connected and does get a room-list, which gets stored as PhotonNetwork.GetRoomList(). This script now calls: PhotonNetwork.JoinRandomRoom();");
-        PhotonNetwork.JoinRoom("WaveDashRun");
+        PhotonNetwork.JoinRandomRoom();
     }
 
-    public virtual void OnPhotonJoinRoomFailed()
+    public virtual void OnPhotonRandomJoinFailed()
     {
         Debug.Log("OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one. Calling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
         PhotonNetwork.CreateRoom("WaveDashRun", new RoomOptions() { MaxPlayers = 100 }, null);
